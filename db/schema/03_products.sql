a2ea64223c5036418fd3d406786f3e0078696e9c
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-11-15 23:37:25 -0500

    Create new tables

---
+++ db/schema/03_products.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS products CASCADE:
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY NOT NULL,
+  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
+  color_id INTEGER REFERENCES colors(id) ON DELETE CASCADE,
+
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  image1 VARCHAR(512),
+  image2 VARCHAR(512),
+  image3 VARCHAR(512),
+  price INTEGER NOT NULL DEFAULT 0,
+  display BOOLEAN NOT NULL DEFAULT false
+);
