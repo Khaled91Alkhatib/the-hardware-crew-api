bb97fbec4ba62368f9062d514fb97c20864ab36c
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-11-20 20:15:28 -0500

    Add new products

--- db/seeds/03_products.sql
+++ db/seeds/03_products.sql
@@ -1,20 +1,122 @@
+-- >> Mice
 INSERT INTO products
 (category_id, color_id, name, description, image1, image2, image3, price, display)
-VALUES (1, 1, 'M65 RGB ULTRA WIRELESS Tunable FPS Gaming Mouse', 'Make all your clicks count with the CORSAIR M65 RGB ULTRA WIRELESS Tunable Gaming Mouse, boasting a durable aluminum frame, hyper-fast SLIPSTREAM WIRELESS, and a 26,000 DPI MARKSMAN optical sensor.','/M65-RGB-ULTRA-01', 'M65-RGB-ULTRA-02', 'M65-RGB-ULTRA-03', 17000, true);
+VALUES (1, 1, 'M65 RGB ULTRA WIRELESS Tunable FPS Gaming Mouse', 'Make all your clicks count with the CORSAIR M65 RGB ULTRA WIRELESS Tunable Gaming Mouse, boasting a durable aluminum frame, hyper-fast SLIPSTREAM WIRELESS, and a 26,000 DPI MARKSMAN optical sensor.','/M65-RGB-ULTRA-01.jpg', '/M65-RGB-ULTRA-02.jpg', '/M65-RGB-ULTRA-03.jpg', 17000, true);
 INSERT INTO products
 (category_id, color_id, name, description, image1, image2, image3, price, display)
-VALUES (1, 2, 'M65 RGB ULTRA WIRELESS Tunable FPS Gaming Mouse', 'Make all your clicks count with the CORSAIR M65 RGB ULTRA WIRELESS Tunable Gaming Mouse, boasting a durable aluminum frame, hyper-fast SLIPSTREAM WIRELESS, and a 26,000 DPI MARKSMAN optical sensor.','/M65-RGB-ULTRA-001', 'M65-RGB-ULTRA-002', 'M65-RGB-ULTRA-003', 17000, false);
+VALUES (1, 2, 'M65 RGB ULTRA WIRELESS Tunable FPS Gaming Mouse', 'Make all your clicks count with the CORSAIR M65 RGB ULTRA WIRELESS Tunable Gaming Mouse, boasting a durable aluminum frame, hyper-fast SLIPSTREAM WIRELESS, and a 26,000 DPI MARKSMAN optical sensor.','/M65-RGB-ULTRA-001.jpg', '/M65-RGB-ULTRA-002.jpg', '/M65-RGB-ULTRA-003.jpg', 17000, false);
 
 INSERT INTO products
 (category_id, color_id, name, description, image1, image2, image3, price, display)
-VALUES (2, 1, 'K55 RGB PRO Gaming Keyboard', 'The CORSAIR K55 RGB PRO Gaming Keyboard lights up your desktop with five-zone dynamic RGB backlighting and powers up your gameplay with six dedicated macro keys.','/K55-RGB-PRO-Keyboard-01', '/K55-RGB-PRO-Keyboard-02', 'K55-RGB-PRO-Keyboard-03', 8000, true);
+VALUES (1, 1, 'KATAR ELITE WIRELESS Gaming Mouse', 'Weighing in at just 69g, the CORSAIR KATAR ELITE WIRELESS gaming mouse packs amazingly agile performance in a compact design that is ideal for claw and fingertip grips.', '/KATAR-ELITE-WIRELESS-01.jpg', '/KATAR-ELITE-WIRELESS-02.jpg', '/KATAR-ELITE-WIRELESS-03.jpg', 12000, true);
 
 INSERT INTO products
 (category_id, color_id, name, description, image1, image2, image3, price, display)
-VALUES (3, 1, 'VOID PRO RGB Wireless Premium Gaming Headset with Dolby® Headphone 7.1', 'The VOID PRO RGB Wireless headset provides exceptional comfort, epic audio performance and legendary CORSAIR durability to deliver the ultimate gaming experience.','/Premium-Gaming-Headset-01', '/Premium-Gaming-Headset-02', '/Premium-Gaming-Headset-03', 12900, true);
+VALUES (1, 1, 'M65 RGB ULTRA Tunable FPS Gaming Mouse', 'Make all your clicks count with the CORSAIR M65 RGB ULTRA Tunable Gaming Mouse, boasting a durable aluminum frame and pinpoint accuracy powered by a 26,000 DPI MARKSMAN optical sensor.', '/M65-RGB-ULTRA-less-01.jpg', '/M65-RGB-ULTRA-less-02.jpg', '/M65-RGB-ULTRA-less-03.jpg', 10000, true);
+
 INSERT INTO products
 (category_id, color_id, name, description, image1, image2, image3, price, display)
-VALUES (3, 2, 'VOID PRO RGB Wireless Premium Gaming Headset with Dolby® Headphone 7.1', 'The VOID PRO RGB Wireless headset provides exceptional comfort, epic audio performance and legendary CORSAIR durability to deliver the ultimate gaming experience.','/Premium-Gaming-Headset-001', '/Premium-Gaming-Headset-002', '/Premium-Gaming-Headset-003', 12900, false);
+VALUES (1, 1, 'SABRE RGB PRO WIRELESS CHAMPION SERIES Ultra-Lightweight FPS/MOBA Gaming Mouse', 'The CORSAIR SABRE RGB PRO WIRELESS Gaming Mouse is designed for and tested by top esports professionals,weighing an ultra-light 79g and equipped with SLIPSTREAM WIRELESS technology for lightning-fast responsiveness.', '/SABRE-RGB-PRO-WIRELESS-01.jpg', '/SABRE-RGB-PRO-WIRELESS-02.jpg', '/SABRE-RGB-PRO-WIRELESS-03.jpg', 15000, true);
 
--- INSERT INTO products (category_id, color_id, name, description, image1, image2, image3, price, display) VALUES ()
--- INSERT INTO products (category_id, color_id, name, description, image1, image2, image3, price, display) VALUES ()
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES (1, 1, 'SCIMITAR PRO RGB Optical MOBA/MMO Gaming Mouse', 'The SCIMITAR PRO RGB gaming mouse advances your game with the customizable Key Slider™ macro button control system, a native 16000 DPI.', '/PRO-BLK-01.jpg', '/PRO-BLK-02.jpg', '/PRO-BLK-03.jpg', 10000, true);
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES (1, 3, 'SCIMITAR PRO RGB Optical MOBA/MMO Gaming Mouse', 'The SCIMITAR PRO RGB gaming mouse advances your game with the customizable Key Slider™ macro button control system, a native 16000 DPI.', '/PRO-BLK-001.jpg', '/PRO-BLK-002.jpg', '/PRO-BLK-003.jpg', 10000, false);
+
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES (1, 1, 'DARK CORE RGB PRO SE Wireless Gaming Mouse', 'The CORSAIR DARK CORE RGB SE gaming mouse uses an ultra-fast 1ms 2.4GHz wireless connection, Qi® wireless charging and a 18,000 DPI optical sensor for true wireless gaming.', '/DARK-CORE-RGB-PRO-01.jpg', '/DARK-CORE-RGB-PRO-02.jpg', '/DARK-CORE-RGB-PRO-03.jpg', 13000, true);
+
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES (1, 1, 'M65 RGB ELITE Tunable FPS Gaming Mouse', 'The CORSAIR M65 RGB ELITE tunable gaming mouse is CORSAIRs most advanced FPS gaming mouse yet, built around a durable aluminum frame and equipped with a state-of-the-art 18,000 DPI optical sensor.', '/M65-RGB-ELITE-01.jpg', '/M65-RGB-ELITE-02.jpg', '/M65-RGB-ELITE-03.jpg', 9000, true);
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES (1, 2, 'M65 RGB ELITE Tunable FPS Gaming Mouse', 'The CORSAIR M65 RGB ELITE tunable gaming mouse is CORSAIRs most advanced FPS gaming mouse yet, built around a durable aluminum frame and equipped with a state-of-the-art 18,000 DPI optical sensor.', '/M65-RGB-ELITE-001.jpg', '/M65-RGB-ELITE-002.jpg', '/M65-RGB-ELITE-003.jpg', 9000, false);
+
+-- INSERT INTO products
+-- (category_id, color_id, name, description, image1, image2, image3, price, display)
+-- VALUES ();
+
+
+-- >> Keyboards
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES (2, 1, 'K55 RGB PRO Gaming Keyboard', 'The CORSAIR K55 RGB PRO Gaming Keyboard lights up your desktop with five-zone dynamic RGB backlighting and powers up your gameplay with six dedicated macro keys.','/K55-RGB-PRO-Keyboard-01.jpg', '/K55-RGB-PRO-Keyboard-02.jpg', '/K55-RGB-PRO-Keyboard-03.jpg', 8000, true);
+
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES (2, 1, 'K70 PRO MINI WIRELESS 60% Mechanical CHERRY MX Red Switch Keyboard with RGB Backlighting - Black', 'The CORSAIR K70 PRO MINI WIRELESS RGB 60% Mechanical Gaming Keyboard is big on both performance and customization, equipped with hyper-fast, sub-1ms SLIPSTREAM WIRELESS and swappable CHERRY MX keyswitches in a portable profile.', '/K70-PRO-MINI-keyboard-01.jpg', '/K70-PRO-MINI-keyboard-02.jpg', '/K70-PRO-MINI-keyboard-03.jpg', 24000, true);
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES (2, 2, 'K70 PRO MINI WIRELESS 60% Mechanical CHERRY MX Red Switch Keyboard with RGB Backlighting - White', 'The CORSAIR K70 PRO MINI WIRELESS RGB 60% Mechanical Gaming Keyboard is big on both performance and customization, equipped with hyper-fast, sub-1ms SLIPSTREAM WIRELESS and swappable CHERRY MX keyswitches in a portable profile.', '/K70-PRO-MINI-keyboard-001.jpg', '/K70-PRO-MINI-keyboard-002.jpg', '/K70-PRO-MINI-keyboard-003.jpg', 24000, false);
+
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES ('2', '1', 'K100 AIR WIRELESS RGB Ultra-Thin Mechanical Gaming Keyboard', 'The CORSAIR K100 AIR Wireless Mechanical Gaming Keyboard delivers outstanding performance with an unbelievably thin design, with CHERRY MX Ultra Low Profile keyswitches and hyper-fast wireless connectivity.', '/K100-AIR-WIRELESS-RGB-01.jpg', '/K100-AIR-WIRELESS-RGB-02.jpg', '/K100-AIR-WIRELESS-RGB-03.jpg', 43000, true);
+
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES ('2', '1', 'K70 RGB MK.2 Low Profile RAPIDFIRE Mechanical Gaming Keyboard', 'The K70 RGB MK.2 Low Profile RAPIDFIRE Mechanical Gaming Keyboard combines the comfort of low-profile, low travel keys, with the performance of ultra-fast CHERRY® MX mechanical keyswitches and stunning RGB lighting.', '/K70-RGB-MK2-01.jpg', '/K70-RGB-MK2-02.jpg', '/K70-RGB-MK2-03.jpg', 24000, true);
+
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES ('2', '1', 'K70 PRO RGB Optical-Mechanical Gaming Keyboard', 'The CORSAIR K70 PRO RGB Optical-Mechanical Gaming Keyboard delivers an iconic aluminum frame and even better performance, equipped with hyper-fast CORSAIR OPX optical-mechanical keyswitches.', '/K70-PRO-OPX-01.jpg', '/K70-PRO-OPX-02.jpg', '/K70-PRO-OPX-03.jpg', 26000, true);
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES ('2', '2', 'K70 PRO RGB Optical-Mechanical Gaming Keyboard', 'The CORSAIR K70 PRO RGB Optical-Mechanical Gaming Keyboard delivers an iconic aluminum frame and even better performance, equipped with hyper-fast CORSAIR OPX optical-mechanical keyswitches.', '/K70-PRO-OPX-001.jpg', '/K70-PRO-OPX-002.jpg', '/K70-PRO-OPX-003.jpg', 26000, false);
+
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES ('2', '1', 'K95 RGB PLATINUM Mechanical Gaming Keyboard', 'CORSAIR K95 RGB PLATINUM features CHERRY® MX Speed keyswitches backed by a lightweight aluminum frame. 8MB profile storage with hardware macro and lighting playback enable profile access independent of external software.', '/K95-GUNMETAL-01.jpg', '/K95-GUNMETAL-02.jpg', '/K95-GUNMETAL-03.jpg', 27000, true);
+
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES ('2', '1', 'K70 LUX Mechanical Gaming Keyboard', 'The K70 LUX has CHERRY® MX Brown switches backed by a lightweight aluminum chassis. CUE enabled advanced lighting control and large font keycaps deliver dynamic, vibrant backlighting.', '/K70-LUX-01.jpg', '/K70-LUX-02.jpg', '/K70-LUX-03.jpg', 18000, true);
+
+
+-- >> Headsets
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES (3, 1, 'VOID PRO RGB Wireless Premium Gaming Headset with Dolby® Headphone 7.1', 'The VOID PRO RGB Wireless headset provides exceptional comfort, epic audio performance and legendary CORSAIR durability to deliver the ultimate gaming experience.','/Premium-Gaming-Headset-01.jpg', '/Premium-Gaming-Headset-02.jpg', '/Premium-Gaming-Headset-03.jpg', 12900, true);
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES (3, 2, 'VOID PRO RGB Wireless Premium Gaming Headset with Dolby® Headphone 7.1', 'The VOID PRO RGB Wireless headset provides exceptional comfort, epic audio performance and legendary CORSAIR durability to deliver the ultimate gaming experience.','/Premium-Gaming-Headset-001.jpg', '/Premium-Gaming-Headset-002.jpg', '/Premium-Gaming-Headset-003.jpg', 12900, false);
+
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES (3, 1, 'CORSAIR Raptor HS30 Analog Gaming Headset', 'Raptor HS30 offers impressive sound quality and a noise cancelling microphone in a comfortable closed back design that is great for extended gaming sessions.', '/Raptor HS30-01.jpg', '/Raptor HS30-02.jpg', '/Raptor HS30-03.jpg', 13000, true);
+
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES (3, 1, 'HS80 RGB USB Wired Gaming Headset', 'The CORSAIR HS80 RGB USB Gaming Headset delivers incredibly detailed sound through custom-tuned 50mm neodymium audio drivers with Dolby Audio® 7.1 surround sound.', '/RGB-USB-BLACK-01.jpg', '/RGB-USB-BLACK-02.jpg', '/RGB-USB-BLACK-03.jpg', 13000, true);
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES (3, 2, 'HS80 RGB USB Wired Gaming Headset', 'The CORSAIR HS80 RGB USB Gaming Headset delivers incredibly detailed sound through custom-tuned 50mm neodymium audio drivers with Dolby Audio® 7.1 surround sound.', '/RGB-USB-BLACK-001.jpg', '/RGB-USB-BLACK-002.jpg', '/RGB-USB-BLACK-003.jpg', 13000, false);
+
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES (3, 1, 'VIRTUOSO RGB WIRELESS XT T-PAIN Edition High-Fidelity Gaming Headset', 'The CORSAIR VIRTUOSO RGB Wireless XT delivers a high-fidelity audio experience, all-day comfort from its premium memory foam earpads, and simultaneous connectivity with hyper-fast SLIPSTREAM WIRELESS and Bluetooth®.', '/VIRTUOSO-CE-TPAIN-01.jpg', '/VIRTUOSO-CE-TPAIN-02.jpg', '/VIRTUOSO-CE-TPAIN-03.jpg', 40000, true);
+
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES (3, 1, 'VIRTUOSO RGB WIRELESS XT High-Fidelity Gaming Headset', 'The CORSAIR VIRTUOSO RGB Wireless XT delivers a high-fidelity audio experience, all-day comfort from its premium memory foam earpads, and simultaneous connectivity with hyper-fast SLIPSTREAM WIRELESS and Bluetooth®.', '/VIRTUOSO-XT-01.jpg', '/VIRTUOSO-XT-02.jpg', '/VIRTUOSO-XT-03.jpg', 36000, true);
+
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES (3, 1, 'HS70 SE WIRELESS Gaming Headset', 'The CORSAIR HS70 SE Wireless Gaming Headset provides exceptional comfort, superior sound quality, a fully detachable microphone, and up to 16 hours of battery life.', '/01-HS70-SE-01.jpg', '/01-HS70-SE-02.jpg', '/01-HS70-SE-03.jpg', 13000, true);
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES (3, 2, 'HS70 SE WIRELESS Gaming Headset', 'The CORSAIR HS70 SE Wireless Gaming Headset provides exceptional comfort, superior sound quality, a fully detachable microphone, and up to 16 hours of battery life.', '/01-HS70-SE-001.jpg', '/01-HS70-SE-002.jpg', '/01-HS70-SE-003.jpg', 13000, false);
+
+INSERT INTO products
+(category_id, color_id, name, description, image1, image2, image3, price, display)
+VALUES (3, 1, 'HS55 WIRELESS CORE Gaming Headset', 'The CORSAIR HS55 WIRELESS CORE Gaming Headset matches low-latency 2.4GHz wireless audio or Bluetooth®connections with lightweight construction, for essential all-day wireless comfort and great sound.', '/HS55-WIRELESS-01.jpg', '/HS55-WIRELESS-02.jpg', '/HS55-WIRELESS-03.jpg', 15000, true);
+
+-- INSERT INTO products (category_id, color_id, name, description, image1, image2, image3, price, display) VALUES ();
+-- INSERT INTO products (category_id, color_id, name, description, image1, image2, image3, price, display) VALUES ();
+-- INSERT INTO products (category_id, color_id, name, description, image1, image2, image3, price, display) VALUES ();
+-- INSERT INTO products (category_id, color_id, name, description, image1, image2, image3, price, display) VALUES ();
+-- INSERT INTO products (category_id, color_id, name, description, image1, image2, image3, price, display) VALUES ();
