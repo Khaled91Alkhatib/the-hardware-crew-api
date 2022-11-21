a5f7d3910307dbe4312bdd3e9ad7f436381d732d
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-11-20 20:15:06 -0500

    Add color

--- db/seeds/02_colors.sql
+++ db/seeds/02_colors.sql
@@ -1,2 +1,3 @@
-INSERT INTO colors (color) VALUES ('white');
 INSERT INTO colors (color) VALUES ('black');
+INSERT INTO colors (color) VALUES ('white');
+INSERT INTO colors (color) VALUES ('yellow');
