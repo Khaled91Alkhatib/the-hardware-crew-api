ea2a5e96680b2fc09c69ea24c9a0abff35f2141f
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-11-23 18:31:43 -0500

    Make seeds first letter uppercase

--- db/seeds/02_colors.sql
+++ db/seeds/02_colors.sql
@@ -1,3 +1,3 @@
-INSERT INTO colors (color) VALUES ('black');
-INSERT INTO colors (color) VALUES ('white');
-INSERT INTO colors (color) VALUES ('yellow');
+INSERT INTO colors (color) VALUES ('Black');
+INSERT INTO colors (color) VALUES ('White');
+INSERT INTO colors (color) VALUES ('Yellow');
