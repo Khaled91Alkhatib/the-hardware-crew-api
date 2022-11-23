7b2bf50d7cc4a12f546813b1e86232a4e52f9a50
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-11-23 18:30:32 -0500

    Add route to indivisual products per id

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllProducts } = require('../db/queries/products/01_getAllProducts')
+const { getAllProducts } = require('../db/queries/products/01_getAllProducts');
+const { getProductById } = require('../db/queries/products/04_getProductById');
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
@@ -11,6 +12,20 @@ module.exports = (db) => {
       .then(([data]) => {
         const products = data.rows;
         res.json({ products });
+        return;
+      })
+      .catch(err => {
+        res.status(500).json(`error: ${err.message}`);
+      });
+  });
+
+  router.get("/:id", (req, res) => {
+    const currentId = req.params.id;
+    const productById = getProductById(db, currentId);
+    Promise.all([productById])
+      .then(([data]) => {
+        const product = data.rows[0];
+        res.json({ product });
         return;
       })
       .catch(err => {
