6a994488f17aab0582b264ab89e5c271c1b68a4c
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-11-28 18:42:49 -0500

    Change the url address and apply module.exports to include several routes

--- routes/stripe.js
+++ routes/stripe.js
@@ -7,70 +7,73 @@ const stripe = require('stripe')(process.env.STRIPE_KEY);
 router.use(express.json());
 require("dotenv").config();
 
-router.post('/create-checkout-session', async (req, res) => {
-  // console.log('req', req)
-  const { cart } = req.body;
-  // console.log('server cart', cart);
-  const line_items = cart.map((item) => {
-    return {
-      price_data: {
-        currency: 'CAD',
-        product_data: {
-          name: item.name,
-          // images: [item.image1],
-          // metadata: {
-          //   SKU: item.sku,
-          //   color: item.color
-          // }
-        },
-        unit_amount: (item.price * 0.13) + item.price,
-      },
-      quantity: item.quantity,
-    };
-  });
+module.exports = () => {
 
-  const session = await stripe.checkout.sessions.create({
-    // line_items: [
-    //   {
-    //     price_data: {
-    //       currency: 'usd',
-    //       product_data: {
-    //         name: 'T-shirt',
-    //       },
-    //       unit_amount: 2000,
-    //     },
-    //     quantity: 1,
-    //   },
-    // ],
-    payment_method_types: ['card'],
-    shipping_address_collection: { allowed_countries: ['US', 'CA'] },
-    shipping_options: [
-      {
-        shipping_rate_data: {
-          type: "fixed_amount",
-          fixed_amount: { amount: 0, currency: 'CAD' },
-          display_name: 'Free shipping',
-          delivery_estimate: {
-            minimum: { unit: 'business_day', value: 5 },
-            maximum: { unit: 'business_day', value: 7 },
+  router.post('/', async (req, res) => {
+    // console.log('req', req)
+    const { cart } = req.body;
+    // console.log('server cart', cart);
+    const line_items = cart.map((item) => {
+      return {
+        price_data: {
+          currency: 'CAD',
+          product_data: {
+            name: item.name,
+            // images: [item.image1],
+            // metadata: {
+            //   SKU: item.sku,
+            //   color: item.color
+            // }
+          },
+          unit_amount: (item.price * 0.13) + item.price,
+        },
+        quantity: item.quantity,
+      };
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      // line_items: [
+      //   {
+      //     price_data: {
+      //       currency: 'usd',
+      //       product_data: {
+      //         name: 'T-shirt',
+      //       },
+      //       unit_amount: 2000,
+      //     },
+      //     quantity: 1,
+      //   },
+      // ],
+      payment_method_types: ['card'],
+      shipping_address_collection: { allowed_countries: ['US', 'CA'] },
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            fixed_amount: { amount: 0, currency: 'CAD' },
+            display_name: 'Free shipping',
+            delivery_estimate: {
+              minimum: { unit: 'business_day', value: 5 },
+              maximum: { unit: 'business_day', value: 7 },
+            },
           },
         },
+      ],
+      phone_number_collection: {
+        enabled: true,
       },
-    ],
-    phone_number_collection: {
-      enabled: true,
-    },
-    line_items,
-    mode: 'payment',
-    success_url: 'http://localhost:3000/thankyou',
-    cancel_url: 'http://localhost:3000/shoppingcart',
+      line_items,
+      mode: 'payment',
+      success_url: 'http://localhost:3000/thankyou',
+      cancel_url: 'http://localhost:3000/shoppingcart',
+    });
+
+    res.send(JSON.stringify({ url: session.url }));
   });
 
-  res.send(JSON.stringify({ url: session.url }));
-});
+  // router.get('/create-checkout-session', (req, res) => {
+  //   res.json(req.body);
+  // });
 
-router.get('/create-checkout-session', (req, res) => {
-  res.json(req.body);
-});
-
-module.exports = router;
+  return router;
+};
