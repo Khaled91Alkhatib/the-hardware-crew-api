e19ce5c5773fec1e57138f65d43d277146b3b928
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-12-04 19:45:40 -0500

    Create new route and implement code to show new added products

--- routes/products.js
+++ routes/products.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { getAllProducts } = require('../db/queries/products/01_getAllProducts');
 const { getProductById } = require('../db/queries/products/04_getProductById');
+const { addNewProduct } = require('../db/queries/products/05_addNewProduct');
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
@@ -27,6 +28,39 @@ module.exports = (db) => {
         const product = data.rows[0];
         res.json({ product });
         return;
+      })
+      .catch(err => {
+        res.status(500).json(`error: ${err.message}`);
+      });
+  });
+
+  router.post("/", (req, res) => {
+    console.log('new product', req.body);
+    const sku = req.body.sku;
+    const category = req.body.category;
+    const color = req.body.color;
+    const name = req.body.name;
+    const description = req.body.description;
+    const image1 = req.body.image1;
+    const image2 = req.body.image2;
+    const image3 = req.body.image3;
+    const price = req.body.price;
+    const display = req.body.display;
+
+    addNewProduct(db, sku, category, color, name, description, image1, image2, image3, price, display)
+      .then(data => {
+        console.log('data', data);
+        // if (sku === "" ||
+        //   category === "" ||
+        //   color === "" ||
+        //   name === "" ||
+        //   description === "" ||
+        //   image1 === null ||
+        //   price === ""
+        // ) {
+        //   res.json({ errCode: 1001, errMsg: `Error: Please fill required fields!` });
+        //   return
+        // }
       })
       .catch(err => {
         res.status(500).json(`error: ${err.message}`);
