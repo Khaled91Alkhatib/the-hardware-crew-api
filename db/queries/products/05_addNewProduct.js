f1a22065ec1f7e015a151b352d88ecf1f4ffb10d
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-12-04 19:45:16 -0500

    Add new parameters for posting new products

--- db/queries/products/05_addNewProduct.js
+++ db/queries/products/05_addNewProduct.js
@@ -1,10 +1,11 @@
-const addNewProduct = (db) => {
+const addNewProduct = (db, sku, category, color, name, description, image1, image2, image3, price, display) => {
   return db.query(`
   INSERT INTO products
   (sku, category_id, color_id, name, description, image1, image2, image3, price, display)
   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
-  RETURNING *;`, []
-  );
+  RETURNING *;`, [sku, category, color,
+    name, description, image1, image2,
+    image3, price, display]);
 };
 
 module.exports = { addNewProduct };
